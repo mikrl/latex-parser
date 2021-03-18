ecfc65ef913f20727b8dacaa32badebe710c67be
Michael <mikrlynch@gmail.com>
2021-03-17 20:24:02 -0400

    Replaced exceptions with error codes in tokenizer. PEP8'd the code.

--- algorithms.py
+++ algorithms.py
@@ -1,53 +1,84 @@
-import queue
+
 import re
 from typing import List
+from typing import Tuple
+
 
 class ShuntingYardError(Exception):
     """Base class for other shunting yard exceptions"""
     pass
 
+
 class UnknownTokenError(Exception):
     """Raised when encountering a token that is not a parenthesis, literal, variable, or operator"""
     pass
 
-class NoOuterParentheses(ShuntingYardError):
+
+class NoOuterParenthesesError(ShuntingYardError):
     """Raised when the input string hasn't been prepared to have outer parens"""
     pass
+
+
+class OperatorsOnStackError(ShuntingYardError):
+    """Raised when the operator stack still holds operators when the algorithm terminates."""
+    pass
+
 
 class MismatchedParenthesesError(ShuntingYardError):
     """Raised when mismatched parentheses lead to an undefined stack operation"""
     pass
 
-def to_token_list(parse_str: str) -> List[str]:
-    # valid_token_regex = re.compile(r'^((OPb|OPu|LIT|VAR)[0-9]+|\(|\))')
+
+def to_token_list(parse_str: str) -> Tuple[List[str], int]:
+    """
+    Transforms the preprocessed parse string into a list of tokens.
+    Also returns a 0 when tokenizing succeeded, or a 1 if it failed.
+
+    Keyword arguments:
+    parse_str -- The preprocessed infix string to be tokenized.
+    """
     valid_token_regex = re.compile(r'^((OP|LIT|VAR)[0-9]+|\(|\))')
     token_list = []
-    while len(parse_str)>0:
+    while len(parse_str) > 0:
         if valid_token_regex.match(parse_str):
             token_end_idx = valid_token_regex.match(parse_str).span()[1]
             next_token = parse_str[:token_end_idx]
             parse_str = parse_str[token_end_idx:]
             token_list.append(next_token)
         else:
-            raise UnknownTokenError("Encountered an unrecognized token!\
-            Lexer didn't catch {0}".format(parse_str))
-    return token_list
+            return ([parse_str], 1)
+
+    return (token_list, 0)
+
 
 def shunting_yard(parse_str: str) -> str:
     """
     Implementation of the shunting yard algorithm.
-    Assumes that the input is parenthesized to resolve precedence ambiguity.
-    Assumes that all literals have been replaced with LITx, where x is a positive integer.
-    Assumes that all variables have been replaced with VARx, where x is a positive integer.
-    Assumes that all operators have been replaced with OPx, where x is a positive integer. 
-    # Assumes that all binary operators have been replaced with OPbx, where x is a positive integer. 
-    # Assumes that all unary operators have been replaced with OPux, where x is a positive integer.
+
+    Assumptions:
+    The input is parenthesized to resolve operator precedence ambiguity.
+    The first and last characters are L and R parens respectively.
+    All literals have been replaced with LITx, where x is a positive integer.
+    All variables have been replaced with VARx, where x is a positive integer.
+    All operators have been replaced with OPx, where x is a positive integer.
+
+    Keyword arguments:
+    parse_str -- The preprocessed infix string to be parsed.
     """
-    
-    in_queue = to_token_list(parse_str)
+
+    if parse_str[0] != "(" and parse_str[-1] != ")":
+        raise NoOuterParenthesesError(
+            "Input as prepared did not have outer parentheses!")
+
+    in_queue, error = to_token_list(parse_str)
     out_queue = []
     op_stack = []
-    
+
+    if error:
+        raise UnknownTokenError(
+            "Encountered an unrecognized token!\
+            Lexer didn't catch {0}".format(in_queue[0]))
+
     for token in in_queue:
         if token[:3] in ['LIT', 'VAR']:
             out_queue.append(token)
@@ -56,14 +87,18 @@ def shunting_yard(parse_str: str) -> str:
         elif token == '(':
             op_stack.append(token)
         elif token == ')':
-            # Pop operators off the stack until we find the matching parenthesis
+            # Pop operators off the stack
+            # until we find the matching parenthesis
             while op_stack[-1] != '(':
                 if len(op_stack) == 0:
-                    raise MismatchedParenthesesError("Emptied the stack while searching for a L_PAREN!")
+                    raise MismatchedParenthesesError(
+                        "Emptied the stack while searching for a L_PAREN!")
                 out_queue.append(op_stack.pop())
-            op_stack.pop() # Pop off the left parenthesis
+            # Pop off the left parenthesis
+            op_stack.pop()
     if len(op_stack) > 0:
-        raise NoOuterParentheses("Input as prepared did not have outer parentheses!")
+        raise OperatorsOnStackError(
+            "Operators left on stack at algorithm termination!")
     return out_queue
 
 
